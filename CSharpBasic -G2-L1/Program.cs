6306224230a765f5f81b7cb8d0e67bb39d398abc
Boban.Kuzmanovski <Boban.Kuzmanovski@PALMYRA04>
2022-03-08 11:31:48 -0800

    Add project files.

---
+++ CSharpBasic -G2-L1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CSharpBasic__G2_L1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            //Console.WriteLine("Zdravo grupa 2!");
+
+            //Console.WriteLine(1);
+            //Console.WriteLine(1.1);
+            //Console.WriteLine(-1);
+            //Console.WriteLine("Zdravo Grupa 2! \nOva e primer vp nova linija");
+            //Console.WriteLine("     *    \n   ****   \n  ******  \n ******** \n**********");
+            //Console.WriteLine("     *");
+            //Console.WriteLine("    ***");
+            //Console.WriteLine("   *****");
+            //Console.WriteLine("  *******");
+            //Console.WriteLine(" *********");
+            //Console.WriteLine("***********");
+            //Console.WriteLine("        *");
+            //Console.WriteLine("        *");
+            //Console.WriteLine("       ***");
+            //Console.WriteLine("     *  *  *");
+            //Console.WriteLine("    **  *  **");
+            //Console.WriteLine("        *");
+            //Console.WriteLine("       * *");
+            //Console.WriteLine("      *   *");
+            //Console.WriteLine("     *     *");
+            //Console.WriteLine("   ***     *** ");
+
+            Console.WriteLine("Please input some text");
+            var input = Console.ReadLine();
+            Console.WriteLine(input);
+        }
+    }
+}
